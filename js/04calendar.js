4856327fa13049394721acffe130c48124e4fa81
guddnboy <hung3018@naver.com>
2024-01-18 17:04:13 +0900

    [04] 캘린더 모양 생성

--- js/04calendar.js
+++ js/04calendar.js
@@ -1,9 +1,41 @@
 const date = new Date();
 
-var currentYear = date.getFullYear();
-var currentMonth = date.getMonth() + 1;
+const currentYear = date.getFullYear();
+const currentMonth = date.getMonth() + 1;
 
 displayDate();
+
+const prevLast = new Date(currentYear, currentMonth - 1,0);
+const thisLast = new Date(currentYear, currentMonth, 0);
+
+const PLDate = prevLast.getDate();
+const PLDay = prevLast.getDay();
+
+const TLDate = thisLast.getDate();
+const TLDay = thisLast.getDay();
+
+const prevDates = [];
+const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+const nextDates = [];
+
+if (PLDay !== 6){
+    for (let i = 0; i < PLDay + 1; i++){
+        prevDates.unshift(PLDate - 1);
+    }
+}
+
+for (let i =1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+}
+
+const dates = prevDates.concat(thisDates,nextDates);
+
+dates.forEach((date,i) => {
+    dates[i] = `<div class="date">${date}</div>`;
+});
+
+document.querySelector('.dates').innerHTML = dates.join('');
+
 
 function displayDate(){
     document.getElementById('currentDate').innerText = currentYear + "년"+ currentMonth + "월";    
