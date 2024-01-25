c8dd927058d647f34b023dfc84444151b46ffb2b
guddnboy <hung3018@naver.com>
2024-01-25 21:08:12 +0900

    [09]사이드 메뉴 표시

--- js/09nav.js
+++ js/09nav.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var menuBtn = document.getElementById('menuBtn');
-    var navMenu = document.querySelector('.nav-menu');
+    var sideMenu = document.querySelector('.side-menu');
 
     menuBtn.addEventListener('click', function () {
-        navMenu.classList.toggle('show-menu');
+        // 토글하여 side-menu 표시 또는 숨김
+        sideMenu.classList.toggle('show-side-menu');
     });
 });
